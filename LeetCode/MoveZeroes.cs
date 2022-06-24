6f65fe0d629a1da6f0c92b7d51b3c48ece67172a
Diogo <dinash@outlook.com>
2022-06-24 14:45:37 -0300

    New Exercises

---
+++ LeetCode/MoveZeroes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class MoveZeroes
+    {
+        public static int[] MoveZeros(int[] nums)
+        {
+            int nonZero = 0;
+            int zero = 0;
+            for(int i =0; i< nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                    nums[nonZero++] = nums[i];
+                else
+                    zero++;
+            }          
+
+            while(zero > 0)
+            {
+                nums[^zero] = 0;
+                zero--;
+            }
+            
+            return nums;
+        }
+    }
+}
---
+++ LeetCode/NextGreaterElementI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class NextGreaterElementI
+    {
+        public static int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            int[] ans = new int[nums1.Length];
+            int higher = 0;
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                higher = int.MaxValue;
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    if (nums1[i] == nums2[j])
+                    {
+                        higher = nums1[i];
+                    }
+                    if (higher < nums2[j])
+                    {
+                        ans[i] = nums2[j];
+                        j = nums2.Length;
+                    }
+                    else if (nums2[j] == nums2[nums2.Length - 1])
+                        ans[i] = -1;
+
+
+                }
+            }
+            return ans;
+        }
+    }
+}
