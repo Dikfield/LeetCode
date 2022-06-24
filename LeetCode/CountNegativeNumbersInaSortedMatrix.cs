94765d049003c40bda5ef1fa43695f59ee16bdbf
Diogo <dinash@outlook.com>
2022-06-24 17:05:02 -0300

    new exercise

---
+++ LeetCode/CountNegativeNumbersInaSortedMatrix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class CountNegativeNumbersInaSortedMatrix
+    {
+        public static int CountNegativeNumbersInaSortedMatrix1(int[][] grid)
+        {
+            int count = 0;
+            for(int i = 0; i < grid.Length; i++)
+            {
+                for( int j = 0; j < grid[i].Length; j++)
+                {
+                    if(grid[i][j] <0)
+                        count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
